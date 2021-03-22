e04ac94d6aa1bd64730e1e005ff945d13f6ada66
MannMoku4 <60726357+manoharmoku4@users.noreply.github.com>
2021-03-22 16:44:41 +0530

    added add method

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,7 +6,8 @@ class HelloWorld{
     System.out.println("Hello World");
   }
  
- public static void add(int num1,int num2){
- System.out.println("num1+num2");
+ public static void add(int num,int num1){
+ System.out.println("num1+num");
+ 
  }
 }
